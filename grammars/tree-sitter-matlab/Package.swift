84c8005ab1a80ceda83cfed2319e50f31e03faa4
eliot-akira <me@eliotakira.com>
2026-01-16 19:27:39 +0100

    git subrepo clone https://github.com/acristoffers/tree-sitter-matlab grammars/tree-sitter-matlab
    subrepo:
      subdir:   "grammars/tree-sitter-matlab"
      merged:   "f3eef66"
    upstream:
      origin:   "https://github.com/acristoffers/tree-sitter-matlab"
      branch:   "main"
      commit:   "f3eef66"
    git-subrepo:
      version:  "0.4.6"

---
+++ grammars/tree-sitter-matlab/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterMatlab",
+    products: [
+        .library(name: "TreeSitterMatlab", targets: ["TreeSitterMatlab"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.8.0"),
+    ],
+    targets: [
+        .target(
+            name: "TreeSitterMatlab",
+            dependencies: [],
+            path: ".",
+            sources: ["src/parser.c", "src/scanner.c"],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterMatlabTests",
+            dependencies: [
+                .product(name: "SwiftTreeSitter", package: "swift-tree-sitter"),
+                "TreeSitterMatlab",
+            ],
+            path: "bindings/swift/TreeSitterMatlabTests"
+        )
+    ],
+    cLanguageStandard: .c11
+)
